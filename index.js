b7298746c2c7f628ec7d4b798b5a2b4f1273252b
Sakeerin <theblue0800@gmail.com>
2025-07-28 14:22:39 +0700

    add other requests

--- index.js
+++ index.js
@@ -14,6 +14,22 @@ app.get("/contact", (req, res) => {
     res.send("<h1>Contact</h1>");
 });
 
+app.post("/register", (req, res) => {
+    res.sendStatus(201);
+});
+
+app.put("/user/update", (req, res) => {
+    res.sendStatus(200);
+});
+
+app.patch("/user/update", (req, res) => {
+    res.sendStatus(200);
+});
+
+app.delete("/delete", (req, res) => {
+    res.sendStatus(200);
+});
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
